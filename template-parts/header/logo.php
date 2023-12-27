26e99e71538b2928095f9ff5fbe03448fb6ad6cd
Matt Vanderpol <matt@vanderpol.net>
2023-12-27 13:37:46 -0800

    Logo partial

---
+++ template-parts/header/logo.php
@@ -0,0 +1,22 @@
+<?php
+$custom_logo_id = \get_theme_mod('custom_logo');
+$title = \get_bloginfo('name');
+?>
+<?php if ( $custom_logo_id ) : ?>
+    <?php 
+        $logo_img_attr = [
+            'class' => 'h-24 w-auto',
+        ];
+        $logo_image = wp_get_attachment_image($custom_logo_id, 'full', false, $logo_img_attr);
+    ?>
+    <div class="flex lg:flex-1">
+        <a 
+            href="<?php echo esc_url( home_url('/') ); ?>"
+            class="-m-1.5 p-1.5"
+            rel="home"
+        >
+            <?php echo $logo_image; ?>
+        </a>
+    </div>
+<?php endif; ?>
+
